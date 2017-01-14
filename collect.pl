ec73e95e55cfa5942eca187b69eb4d811728d1e1
Mark Stringer <mark.stringer@mumbly.co.uk>
2017-01-14 14:15:13 +0000

    add a script to put all the text files together

---
+++ collect.pl
@@ -0,0 +1,23 @@
+use strict;
+ 
+my $debug = 1;
+
+my $orderFile = shift;
+
+##$debug && print $orderFile."\n";
+
+## load in the order filea
+open(ORDER, '<:encoding(UTF-8)', $orderFile) || die "Failed to open text file";
+my $saf = $/;
+
+while (<ORDER>)
+{
+	open (FILE, $_) || die "failed to open file";
+	$/ = undef;
+	print <FILE>;
+	$/ = $saf;
+}
+
+## loop through the files and concatenate all of the files
+
+## output the file to stdout
