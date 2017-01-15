cbef12150ecff658643a4f1f01b4786b3afdd04e
Mark Stringer <mark.stringer@mumbly.co.uk>
2017-01-15 00:07:06 +0000

    Make the shell script deal with a command line argument

--- makebook.sh
+++ makebook.sh
@@ -1,4 +1,7 @@
-#! /bin/bash
-perl collect.pl order.txt > out.txt
+#!/bin/bash
+
+FILE="$1";
+perl collect.pl "$FILE" > out.txt
 perl txt2Markdown.pl out.txt > out.htm
 perl html2pdf.pl out.htm out.pdf
+perl md2epub.pl out.txt out.epub
